1fb1f50cd0e5b39bd227f25b9fa9ae73ef5b1e4a
filip-si <filip270697@gmail.com>
2021-12-09 00:51:23 +0100

    properties and startup

--- Sourceful.Task/Startup.cs
+++ Sourceful.Task/Startup.cs
@@ -1,12 +1,12 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Sourceful.Infrastructure;
 
 namespace Sourceful.Task
 {
@@ -22,12 +22,21 @@ namespace Sourceful.Task
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<DbSourcefulTask>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMediatR(typeof(Startup).Assembly);
+            services.AddControllers().AddNewtonsoftJson(options =>
+                                                        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
