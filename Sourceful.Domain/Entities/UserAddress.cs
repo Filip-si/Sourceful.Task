a22300abfd8a063451883be8e8a3b5a092691601
filip-si <filip270697@gmail.com>
2021-12-08 15:46:50 +0100

    Clean Architecture, create three tables User, UserAddress, UserSetting and Dbcontext

---
+++ Sourceful.Domain/Entities/UserAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sourceful.Domain.Entities
+{
+    public class UserAddress
+    {
+        public Guid UserAddressId { get; set; }
+
+        public string StreetName { get; set; }
+
+        public int Number { get; set; }
+
+        public int PostCode { get; set; }
+    }
+}
---
+++ Sourceful.Infrastructure/DbSourcefulTask.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sourceful.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sourceful.Infrastructure
+{
+    public class DbSourcefulTask : DbContext
+    {
+        public DbSourcefulTask(DbContextOptions<DbSourcefulTask> options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserAddress> UserAddresses { get; set; }
+
+        public DbSet<UserSetting> UserSettings { get; set; }
+    }
+}
